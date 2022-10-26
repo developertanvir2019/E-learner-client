11139c14161e2867ab3722fe23ee5a37dbcfca4d
tanvir <tanvir101tk@gmail.com>
2022-10-26 15:07:32 +0600

    component added

---
+++ src/Others/AuthProvider.js
@@ -0,0 +1,49 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
+import app from '../firebase/Firebase.config';
+export const AuthContext = createContext()
+const auth = getAuth(app)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const providerLogIn = (provider) => {
+        return signInWithPopup(auth, provider)
+    }
+    const emailVerify = () => {
+        return sendEmailVerification(auth.currentUser)
+    }
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const updateUserProfile = (profile) => {
+        return updateProfile(auth.currentUser, profile)
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (user === null || user.emailVerified) {
+                setUser(currentUser);
+            }
+            setLoading(false);
+        });
+        return () => unsubscribe;
+    }, [])
+    const authInfo = { providerLogIn, user, logOut, createUser, signIn, loading, updateUserProfile, emailVerify }
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Others/Router.js
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../component/Home/Home";
+import Main from "../component/Layout/Main";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/category/:id',
+                element: <Categories></Categories>,
+                loader: ({ params }) => fetch(`https://serverite-developertanvir2019.vercel.app/category/${params.id}`)
+            },
+            {
+                path: '/news/:id',
+                element: <PrivateRoute><News></News></PrivateRoute>,
+                loader: ({ params }) => fetch(`https://serverite-developertanvir2019.vercel.app/news/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/term',
+                element: <TermsAndCondition></TermsAndCondition>
+            },
+            {
+                path: '/profile',
+                element: <PrivateRoute><Profile></Profile></PrivateRoute>
+            },
+        ]
+    }
+])
